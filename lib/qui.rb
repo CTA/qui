56017cbd591ef40160fb5c349aa9265e506aa313
JC Wilcox <84jwilcox@gmail.com>
2014-02-19 10:20:16 -0600

    Add Location
    ActiveDirectory class for interacting with the locations
    table in Queuemetrics

--- lib/qui.rb
+++ lib/qui.rb
@@ -6,5 +6,6 @@ require "qui/base"
 require "qui/user"
 require "qui/agent"
 require "qui/queue"
+require "qui/location"
 
 module Qui end
---
+++ lib/qui/location.rb
@@ -0,0 +1,5 @@
+module Qui
+  class Location < ActiveRecord::Base
+    self.table_name = 'locations'
+  end
+end
